d849c05e04fee84b596ade654e0ad90676022aa0
Okashe <okashabdi88@gmail.com>
2024-05-27 21:55:18 -0500

    destructure validated fields from register form

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,8 @@
 "use server"
+
 import * as z from "zod"
+import bcrypt from 'bcrypt'
+
 import { RegisterSchema } from "@/schemas"
 
 export const register = async (values:z.infer<typeof RegisterSchema>)=>{
@@ -9,6 +12,9 @@ export const register = async (values:z.infer<typeof RegisterSchema>)=>{
     if(!validatedFields.success){
         return { error: "Inavalid Fields!" }
     }
+
+    const {name, email, password} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     return { success: "Email sent!" }
 }
